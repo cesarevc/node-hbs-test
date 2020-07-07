9feb43f697638a263d493667aee1198de4c6ac66
Vazquez Cesar <cesar.evc@outlook.com>
2020-07-06 21:16:56 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+// middleware
+app.use( express.static(__dirname + '/public'));
+
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'dev cesc',
+    });
+})
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+})
+
+app.listen(3000, () => {
+    console.log('escuchando en el puerto 3000')
+});
